1f91d18ca96718155656c5cb121f5b0573ecf4b8
Viktoriya Ezhova <vicezhova@gmail.com>
2022-01-30 20:35:13 +0300

    Домашнее задание 3 - Исправления 1

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -18,7 +18,7 @@
  */
 
 function isAllTrue(array, fn) {
-  if (!Array.isArray(array) || array.length === 0) {
+  if (!(array instanceof Array) || array.length === 0) {
     throw new Error('empty array');
   } else if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
@@ -46,7 +46,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (!Array.isArray(array) || array.length === 0) {
+  if (!(array instanceof Array) || array.length === 0) {
     throw new Error('empty array');
   } else if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
